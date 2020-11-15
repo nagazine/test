875c65b98636435781a1ed7aa6f989096ef83068
Yuta Nakajima <nakajima016@gmail.com>
2020-11-16 01:24:48 +0900

    5,6,7追加

---
+++ 07_main.js
@@ -0,0 +1,52 @@
+
+// - global -------------------------------------------------------------------
+var screenCanvas, info,ctx;
+var num = 0;
+
+//ローカルストレージからの読み込み
+if(window.localStorage){
+    num = parseInt(window.localStorage.getItem('Num'));
+    if(!num)num=0;    
+}
+
+// - main ---------------------------------------------------------------------
+window.onload = function(){
+
+    // スクリーンの初期化
+    screenCanvas = document.getElementById('screen');
+    screenCanvas.width = 500;
+    screenCanvas.height = 500;
+
+    // エレメント関連
+    info = document.getElementById('info');
+
+    ctx = screenCanvas.getContext('2d');
+    button(400,400,50,50);
+};
+
+function button(x,y,width,height){
+    //矩形を描画
+    ctx.rect(x, y, width, height);
+    ctx.stroke();
+    //canvas内クリック時イベント
+    screenCanvas.addEventListener('click', function(e){
+        var button = e.target.getBoundingClientRect();//canvasの矩形サイズ取得
+        mouseX = e.clientX - button.left;//canvasに対するクリック位置の相対値を取得
+        mouseY = e.clientY - button.top;
+        //マウス位置がボタン内部にいればボタン押下時処理を実施
+        if(x < mouseX && mouseX < x + width){
+            if(y < mouseY && mouseY < y + height){
+                // alert("mouseX:"+mouseX+" mouseY:"+mouseY+"\n"
+                // +"e.clientX:"+e.clientX+" e.clientY:"+e.clientY);
+                ctx.clearRect(90,90,110,110);//指定範囲のcanvas表示クリア
+                num++;
+                ctx.fillText(num,100,100)//カウンタ数値を表示
+
+                //ローカルストレージへの書き込み
+                if(window.localStorage){
+                    window.localStorage.setItem('Num',num);
+                }
+            }
+        }
+    }, false);
+}
